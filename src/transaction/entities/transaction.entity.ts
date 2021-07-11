6e48324d9b04e47a62a36b8aeec9d72798c8e4db
Ariel Gordon <arielgordon123@gmail.com>
2021-07-11 13:23:29 +0300

    implement transaction route

--- src/transaction/entities/transaction.entity.ts
+++ src/transaction/entities/transaction.entity.ts
@@ -1,1 +1,20 @@
-export class Transaction {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+export type TransactionDocument = Transaction & Document;
+
+@Schema()
+export class Transaction {
+   
+    @Prop()
+    date: Date;
+
+    @Prop()
+    productList: [{_id, count}]
+}
+
+export const TransactionSchema = SchemaFactory.createForClass(Transaction);
+// TransactionSchema.pre('save', function (next) {
+//     console.log('this :>> ', this);
+//     next()
+//   })
--- src/transaction/transaction.service.ts
+++ src/transaction/transaction.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel,  Schema as MongooseSchema } from '@nestjs/mongoose';
+import * as mongoose from 'mongoose';
+
 import { CreateTransactionDto } from './dto/create-transaction.dto';
 import { UpdateTransactionDto } from './dto/update-transaction.dto';
+import { Transaction, TransactionDocument } from './entities/transaction.entity';
 
 @Injectable()
 export class TransactionService {
+
+  constructor(@InjectModel(Transaction.name) private transactionModel: mongoose.Model<TransactionDocument>) {}
+
   create(createTransactionDto: CreateTransactionDto) {
     return 'This action adds a new transaction';
   }
